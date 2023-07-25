cea13897eea054b22657ed4c7a398e0ba91499f0
zaydanr <zaydanremtulla@gmail.com>
2023-07-25 10:08:39 -0400

    Initial commit

---
+++ lib/camera.dart
@@ -0,0 +1,49 @@
+import "package:flutter/material.dart";
+import "package:youtube_player_flutter/youtube_player_flutter.dart";
+
+class Camera extends StatefulWidget {
+  const Camera({Key? key}) : super(key : key);
+
+  @override
+  State<Camera> createState() => _CameraState();
+}
+
+class _CameraState extends State<Camera> {
+  final videoURL = "https://www.youtube.com/watch?v=jfKfPfyJRdk";
+  late YoutubePlayerController _controller;
+
+  @override
+
+  void initState() {
+    // TODO: implement initState
+    final videoID = YoutubePlayer.convertUrlToId(videoURL);
+
+    _controller = YoutubePlayerController(
+      initialVideoId: videoID!,
+      flags: const YoutubePlayerFlags(
+        autoPlay: true,
+        isLive: true,
+        controlsVisibleAtStart: false,
+        showLiveFullscreenButton: false,
+      )
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Camera Feed")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          YoutubePlayer(
+            controller: _controller,
+            showVideoProgressIndicator: false,
+            ),
+        ],
+      )
+      );
+      
+  }
+}
